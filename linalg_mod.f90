a48cb01acf1f62b91c7abc4a77dcb57d3a104bbd
Daniel C. Herman <hermda02@luther.edu>
2020-08-03 17:06:09 +0200

    sparse matrix multiplier added and tested

--- linalg_mod.f90
+++ linalg_mod.f90
@@ -204,6 +204,193 @@ contains
 
     end subroutine compute_cg
 
+    function mm_mpi(A,B)
+      implicit none
+
+      integer(i4b)   :: nworkers, source, dest, mtype
+      integer(i4b)   :: cols, avecol, extra, offset
+      integer(i4b)   :: m, n, p, ii, ij, ik
+      integer status(mpi_status_size)
+
+      real(dp), dimension(:,:), intent(in)  :: A
+      real(dp), dimension(:,:), intent(in)  :: B
+      real(dp), dimension(size(A(:,1)),size(B(1,:))) :: mm_mpi
+
+      m = size(A(:,1)) !number of rows in A
+      n = size(A(1,:)) !number of columns in A (rows of B)
+      p = size(B(1,:)) !number of columns of B 
+
+      if (size(A(1,:)) /= size(B(:,1))) then
+         if (rank == master) then
+            write(*,*) 'Matrix sizes do not conform!'
+            stop
+         end if
+      end if
+
+      nworkers = numprocs - 1
+
+      !----------------------- MASTER TASK ------------------------
+      if (rank == master) then
+
+         ! Send out matrix information to the workers
+         avecol = p/nworkers
+         extra  = mod(p,nworkers)
+         offset = 1
+         mtype  = FROM_MASTER
+
+         do dest=1,nworkers
+            if (dest .le. extra) then
+               cols = avecol + 1
+            else
+               cols = avecol
+            end if
+            write(*,*) '  sending ', cols, ' cols to task ', dest
+            call mpi_send(offset, 1, MPI_INTEGER, dest, mtype, MPI_COMM_WORLD, ierr)
+            call mpi_send(cols, 1, mpi_integer, dest, mtype, mpi_comm_world, ierr)
+            call mpi_send(a, m*n, mpi_double_precision, dest, mtype, mpi_comm_world, ierr)
+            call mpi_send(b(1,offset), cols*n, mpi_double_precision, dest, mtype, &
+                      mpi_comm_world, ierr)
+            offset = offset + cols
+         end do
+
+         ! Aaand receive results from the workers
+         mtype = from_worker
+         do ii=1, nworkers
+            source = ii
+            call mpi_recv(offset, 1, mpi_integer, source, mtype, mpi_comm_world, status, ierr)
+            call mpi_recv(cols, 1, mpi_integer, source, mtype, mpi_comm_world, status, ierr)
+            call mpi_recv(mm_mpi(1,offset), cols*m, mpi_double_precision, source, mtype, &
+                      mpi_comm_world, ierr)
+         end do
+      end if
+
+      ! ------------------- WORKER TASK ---------------------------
+  
+      if (rank > master) then
+         ! Receive data from master
+         mtype = from_master
+         call mpi_recv(offset, 1, mpi_integer, master, mtype, mpi_comm_world, status, ierr)
+         call mpi_recv(cols, 1, mpi_integer,  master, mtype, mpi_comm_world, status, ierr)
+         call mpi_recv(a, m*n, mpi_double_precision, master, mtype, mpi_comm_world, status, ierr)
+         call mpi_recv(b, cols*n, mpi_double_precision, master, mtype, mpi_comm_world, status, ierr)
+
+         ! Multiply matrices
+         do ik = 1, cols
+            do ii = 1, m
+               mm_mpi(ii,ik) = 0.0
+               do ij=1, n
+                  mm_mpi(ii,ik) = mm_mpi(ii,ik) + a(ii,ij)*b(ij,ik)
+               end do
+            end do
+         end do
+         
+         mtype = from_worker
+         ! and send the results back
+         call mpi_send(offset, 1, mpi_integer, master, mtype, mpi_comm_world, ierr)
+         call mpi_send(cols, 1, mpi_integer, master, mtype, mpi_comm_world, ierr)
+         call mpi_send(mm_mpi, cols*m, mpi_double_precision, master, mtype, mpi_comm_world, ierr)
+      end if
+    end function mm_mpi
+
+    function compute_ATA(mat,type) result(B)
+      implicit none
+      real(dp), dimension(:,:), intent(in)  :: mat
+      character(len=3), intent(in)          :: type
+      integer(i4b)                          :: ii, ij, ik, m, n
+      real(dp), allocatable, dimension(:,:) :: B
+      real(dp)                              :: q
+      
+      ! Little function to compute the product of a matrix with its transpose
+      ! saves time since the matrix is symmetric!
+
+      m = size(mat(1,:))
+      n = size(mat(:,1))
+
+      if (trim(type) == 'ata') then
+         allocate(B(m,m))
+         ! Compute A^T A
+         B(:,:) = 0.d0
+
+!         call mpi_bcast()
+
+         do ii = 1, m
+            do ij = ii, m
+               q = 0.d0
+               do ik = 1, n
+                  q = q + mat(ik,ii)*mat(ik,ij)
+               end do
+               B(ii,ij) = q
+               B(ij,ii) = q
+            end do
+         end do
+
+
+!      else if (trim(type) == 'aat') then
+         ! Compute A A^T
+!         allocate(B(n,n))
+
+      end if
+
+    end function compute_ATA
+
+    function compute_sparse_ATA(spar,type,n,m) result(B)
+      implicit none
+      real(dp), dimension(:,:),   intent(in) :: spar
+      character(len=3),           intent(in) :: type
+      integer(i4b),               intent(in) :: n, m
+      integer(i4b)                           :: i, l
+      integer(i4b)                           :: ii, ij, ik
+      real(dp), allocatable, dimension(:,: ) :: B
+      real(dp)                               :: q, row, col
+      
+      ! Little function to compute the product of a matrix with its transpose
+      ! saves time since the matrix is symmetric!
+
+      l = n
+
+      if (trim(type) == 'ata') then
+         allocate(B(m,m))
+         ! Compute A^T A
+         B(:,:) = 0.d0
+         do i = 1, l
+            col = spar(i,2)
+            if (spar(i,2) == spar(i-1,2)) then
+               cycle
+            end if
+            ij  = i
+            do while (ij < l+1 .and. spar(ij,2) == col)
+               ii  = 1
+               q = 0
+               do while (ii < l+1)
+                  if (spar(ii,1) > spar(ii-1,1)) then
+                     row = spar(ii,2)
+                     do while (spar(ii,1) > spar(ii-1,1))
+                        if (spar(ii,1) == spar(ij,1)) then
+                           q = q + spar(ii,3)*spar(ij,3)
+                        end if
+                        if (ii == l) b(row,col) = b(row,col) + q
+                        ii = ii + 1
+                     end do
+                  else if (spar(ii,1) <= spar(ii-1,1)) then
+                     !              write(*,fmt='(a,2(i3),f8.2)') 'row, col, x ', row, col, x
+                     !              write(*,fmt='(a,2(i3))') 'ii, ij', ii, ij
+                     b(row,col) = b(row,col) + q
+                     q          = 0
+                     row = spar(ii,2)
+                     if (spar(ii,1) == spar(ij,1)) then
+                        q = q + spar(ii,3)*spar(ij,3)
+                     end if
+                     ii = ii + 1
+                  end if
+               end do
+               ij = ij + 1          
+            end do
+         end do
+
+      end if
+
+    end function compute_sparse_ATA
+
     function compute_ATA_CSC(v,row_i,col_p) result(B)
       implicit none
       real(dp), dimension(:),     intent(in) :: v
