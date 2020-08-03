a0b1bbeed557ad507d365dd06f146748ebc5bfdd
Daniel C. Herman <hermda02@luther.edu>
2020-08-03 16:54:37 +0200

    adding

--- init_mod.f90
+++ init_mod.f90
@@ -1,5 +1,6 @@
 module init_mod
     use healpix_types
+    use utility_mod
     implicit none
 
     ! Constants
@@ -7,10 +8,24 @@ module init_mod
     real(dp)           :: h       = 1.0545726691251021d-34 * 2.d0 * pi
     real(dp)           :: c       = 2.99792458d8
     real(dp)           :: T_CMB   = 2.7255d0
-
+    real(dp)           :: t1, t2, t3
     integer(i4b)       :: nbands
+    integer(i4b)       :: master      = 0 
+    integer(i4b)       :: from_master = 1
+    integer(i4b)       :: from_worker = 2
+
 
 contains 
+
+    subroutine init_mpi()
+      call mpi_init(ierr)
+      call mpi_comm_rank(MPI_COMM_WORLD, rank, ierr)
+      call mpi_comm_size(MPI_COMM_WORLD, numprocs, ierr)
+      if (rank == 0) then
+         write(*,'(a,i8)') ' The number of processors available = ', numprocs
+      end if
+    end subroutine init_mpi
+
 
     function getlun()
         implicit none
