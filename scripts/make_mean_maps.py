6f5fd95638fcbc4e5ea93ec4e7b535a7a0b4e1ff
hermda02 <hermda02@luther.edu>
2020-11-02 22:57:25 +0100

    script update

---
+++ scripts/make_mean_maps.py
@@ -0,0 +1,199 @@
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+import os
+
+
+def return_mean_map(list,outfile):
+    nside = 64
+    npix  = nside*nside*12
+    samples = len(list)
+
+    print(samples)
+
+    maps = np.empty((npix,samples))
+    out_map = np.empty(npix)
+    
+    maps = maps.T
+    
+    for i in range(samples):
+        maps[i] = hp.read_map('../'+dir+'/'+list[i])
+
+    maps = maps.T
+
+    for i in range(npix):
+        out_map[i] = np.mean(maps[i][:])
+
+    hp.write_map('../'+dir+'/'+str(outfile),out_map)
+
+def return_std_map(list,outfile):
+    nside = 64
+    npix  = nside*nside*12
+    samples = len(list)
+
+    print(samples)
+
+    maps = np.empty((npix,samples))
+    out_map = np.empty(npix)
+    
+    maps = maps.T
+    
+    for i in range(samples):
+        maps[i] = hp.read_map('../'+dir+'/'+list[i])
+
+    maps = maps.T
+
+    for i in range(npix):
+        out_map[i] = np.std(maps[i][:])
+
+    hp.write_map('../'+dir+'/'+outfile,out_map)
+    
+def read_params(filename):
+    labels = []
+    freq   = []
+    with open(filename,'r') as infile:
+        for line in infile:
+            if line.startswith('NUMBAND'):
+                numbands = int(line.split('=')[1])
+            if line.startswith('NUMGIBBS'):
+                numgibbs = int(line.split('=')[1][:5])
+                
+    blabs = []
+    bfreq = []
+    for band in range(numbands):
+        blabs.append('BAND_LABEL'+str(band+1).zfill(2))
+        bfreq.append('BAND_FREQ'+str(band+1).zfill(2))        
+    for band in range(numbands):
+         with open(filename,'r') as infile:
+            for line in infile:
+                if line.startswith(blabs[band]):
+                    name = str.strip(line.split('=')[1])
+                    labels.append(name)
+                if line.startswith(bfreq[band]):
+                    fre  = str.strip(line.split('=')[1])
+                    freq.append(float(fre))
+    return labels, freq, numgibbs
+    
+dir = str(input('Which directory to mean? '))
+
+files = os.listdir('../'+dir)
+
+names, freq, num_samp = read_params('../'+dir+'/param_'+dir+'.txt')
+labels = [name.replace("'","") for name in names]
+# labels = [w.replace("_","\_") for w in labels]
+num_bands = len(freq)
+print(labels)
+print(freq)
+
+res_1_Q = []
+res_2_Q = []
+res_3_Q = []
+res_4_Q = []
+res_5_Q = []
+
+res_1_U = []
+res_2_U = []
+res_3_U = []
+res_4_U = []
+res_5_U = []
+
+synch_Qs = []
+synch_Us = []
+chi_Qs   = []
+chi_Us   = []
+
+beta_ss  = []
+
+for file in files:
+    
+    # Residuals Q
+    
+    if file.startswith(labels[0]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_1_Q.append(file)
+
+    if file.startswith(labels[1]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_2_Q.append(file)
+    
+    if file.startswith(labels[2]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_3_Q.append(file)
+
+    if file.startswith(labels[3]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_4_Q.append(file)
+
+    if file.startswith(labels[4]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_5_Q.append(file)
+
+    # Residuals U
+    
+    if file.startswith(labels[0]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_1_U.append(file)
+
+    if file.startswith(labels[1]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_2_U.append(file)
+    
+    if file.startswith(labels[2]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_3_U.append(file)
+
+    if file.startswith(labels[3]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_4_U.append(file)
+
+    if file.startswith(labels[4]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_5_U.append(file)
+            
+    # Synch maps
+            
+    if file.startswith('bp_044_synch_amplitude_Q'):
+        if file.endswith('.fits'):
+            synch_Qs.append(file)
+            
+    if file.startswith('bp_044_synch_amplitude_U'):
+        if file.endswith('.fits'):
+            synch_Us.append(file)
+
+    # Synch beta
+    if file.startswith('synch_beta_Q'):
+        if file.endswith('.fits'):
+            beta_ss.append(file)
+            
+    # Chisquare maps
+
+    if file.startswith('chisq_Q'):
+        if file.endswith('.fits'):
+            chi_Qs.append(file)
+
+    if file.startswith('chisq_U'):
+        if file.endswith('.fits'):
+            chi_Us.append(file)
+
+
+return_mean_map(chi_Qs,'chisq_Q_mean.fits')
+return_mean_map(chi_Us,'chisq_U_mean.fits')
+return_mean_map(synch_Qs,'synch_Q_mean.fits')
+return_mean_map(synch_Us,'synch_U_mean.fits')
+
+return_mean_map(beta_ss,'synch_beta_mean.fits')
+
+return_std_map(synch_Qs,'synch_Q_std.fits')
+return_std_map(synch_Us,'synch_U_std.fits')
+
+return_mean_map(res_1_Q,labels[0]+'_residual_Q_mean.fits')
+return_mean_map(res_2_Q,labels[1]+'_residual_Q_mean.fits')
+return_mean_map(res_3_Q,labels[2]+'_residual_Q_mean.fits')
+return_mean_map(res_4_Q,labels[3]+'_residual_Q_mean.fits')
+return_mean_map(res_5_Q,labels[4]+'_residual_Q_mean.fits')
+
+return_mean_map(res_1_U,labels[0]+'_residual_U_mean.fits')
+return_mean_map(res_2_U,labels[1]+'_residual_U_mean.fits')
+return_mean_map(res_3_U,labels[2]+'_residual_U_mean.fits')
+return_mean_map(res_4_U,labels[3]+'_residual_U_mean.fits')
+return_mean_map(res_5_U,labels[4]+'_residual_U_mean.fits')
