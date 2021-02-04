6e99ad900575e17421237c69387d4fca435f749b
hermda02 <hermda02@luther.edu>
2021-02-05 00:12:47 +0100

    synch it up

--- scripts/make_mean_maps.py
+++ scripts/make_mean_maps.py
@@ -2,7 +2,7 @@ import numpy as np
 import healpy as hp
 import matplotlib.pyplot as plt
 import os
-
+import sys
 
 def return_mean_map(list,outfile):
     print('Creating '+str(outfile))
@@ -63,8 +63,8 @@ def read_params(filename):
     blabs = []
     bfreq = []
     for band in range(numbands):
-        blabs.append('BAND_LABEL'+str(band+1).zfill(2))
-        bfreq.append('BAND_FREQ'+str(band+1).zfill(2))        
+        blabs.append('BAND_LABEL'+str(band+1).zfill(3))
+        bfreq.append('BAND_FREQ'+str(band+1).zfill(3))        
     for band in range(numbands):
          with open(filename,'r') as infile:
             for line in infile:
@@ -75,17 +75,19 @@ def read_params(filename):
                     fre  = str.strip(line.split('=')[1])
                     freq.append(float(fre))
     return labels, freq, numgibbs
-    
-dir = str(input('Which directory to mean? '))
 
-files = os.listdir('../'+dir)
+try:    
+    dir = sys.argv[1] #str(input('Which directory to mean? '))
+    files = os.listdir('../'+dir)
+    names, freq, num_samp = read_params('../'+dir+'/param_'+dir+'.txt')
+    labels = [name.replace("'","") for name in names]
+    num_bands = len(freq)
+    print(labels)
+    print(freq)
 
-names, freq, num_samp = read_params('../'+dir+'/param_'+dir+'.txt')
-labels = [name.replace("'","") for name in names]
-# labels = [w.replace("_","\_") for w in labels]
-num_bands = len(freq)
-print(labels)
-print(freq)
+except:
+    print("Input which directory you wish to point to (../ automatically included)")
+    exit()
 
 res_1_Q = []
 res_2_Q = []
@@ -93,6 +95,8 @@ res_3_Q = []
 res_4_Q = []
 res_5_Q = []
 res_6_Q = []
+res_7_Q = []
+res_8_Q = []
 
 res_1_U = []
 res_2_U = []
@@ -100,6 +104,8 @@ res_3_U = []
 res_4_U = []
 res_5_U = []
 res_6_U = []
+res_7_U = []
+res_8_U = []
 
 synch_Qs = []
 synch_Us = []
@@ -136,6 +142,14 @@ for file in files:
         if file.endswith('.fits'):
             res_6_Q.append(file)
 
+    if file.startswith(labels[6]+'_residual_Q'):
+        if file.endswith('.fits'):
+            res_7_Q.append(file)
+
+#    if file.startswith(labels[7]+'_residual_Q'):
+#        if file.endswith('.fits'):
+#            res_8_Q.append(file)
+
     # Residuals U
     
     if file.startswith(labels[0]+'_residual_U'):
@@ -161,6 +175,14 @@ for file in files:
     if file.startswith(labels[5]+'_residual_U'):
         if file.endswith('.fits'):
             res_6_U.append(file)
+
+    if file.startswith(labels[6]+'_residual_U'):
+        if file.endswith('.fits'):
+            res_7_U.append(file)
+            
+    # if file.startswith(labels[7]+'_residual_U'):
+    #     if file.endswith('.fits'):
+    #         res_8_U.append(file)
             
     # Synch maps
             
@@ -198,12 +220,16 @@ return_mean_map(beta_ss,'synch_beta_mean.fits')
 return_std_map(synch_Qs,'synch_Q_std.fits')
 return_std_map(synch_Us,'synch_U_std.fits')
 
+return_std_map(beta_ss,'synch_beta_std.fits')
+
 return_mean_map(res_1_Q,labels[0]+'_residual_Q_mean.fits')
 return_mean_map(res_2_Q,labels[1]+'_residual_Q_mean.fits')
 return_mean_map(res_3_Q,labels[2]+'_residual_Q_mean.fits')
 return_mean_map(res_4_Q,labels[3]+'_residual_Q_mean.fits')
 return_mean_map(res_5_Q,labels[4]+'_residual_Q_mean.fits')
 return_mean_map(res_6_Q,labels[5]+'_residual_Q_mean.fits')
+return_mean_map(res_7_Q,labels[6]+'_residual_Q_mean.fits')
+#return_mean_map(res_8_Q,labels[7]+'_residual_Q_mean.fits')
 
 return_mean_map(res_1_U,labels[0]+'_residual_U_mean.fits')
 return_mean_map(res_2_U,labels[1]+'_residual_U_mean.fits')
@@ -211,3 +237,5 @@ return_mean_map(res_3_U,labels[2]+'_residual_U_mean.fits')
 return_mean_map(res_4_U,labels[3]+'_residual_U_mean.fits')
 return_mean_map(res_5_U,labels[4]+'_residual_U_mean.fits')
 return_mean_map(res_6_U,labels[5]+'_residual_U_mean.fits')
+return_mean_map(res_7_U,labels[6]+'_residual_U_mean.fits')
+#return_mean_map(res_8_U,labels[7]+'_residual_U_mean.fits')
